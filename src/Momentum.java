f2d8871aa139cee487109d7644e6112c7d1b310a
trev8250 <trev8250@ISS147D8-12.scdsb.pri>
2015-09-11 10:25:24 -0400

    Initial Commit

--- src/Momentum.java
+++ src/Momentum.java
@@ -1,9 +1,17 @@
+import hsa.*;
 
 public class Momentum {
 
     
     public static void main(String[] args) {
-       
+       Console c = new Console();
+       double mass,v,mm;
+       c.print("Enter objects mass: ");
+       mass = c.readDouble();
+       c.print("Enter objects velocity: ");
+       v = c.readDouble();
+       mm = mass * v;
+       c.print("The momentum of the object is " + mm);
     }
     
 }
