ab51db9b29a073c9e50584f10a6239e776c5a7ff
trev8250 <trev8250@ISS147D8-12.scdsb.pri>
2015-09-14 10:36:45 -0400

    pizza

--- src/BarGraph.java
+++ src/BarGraph.java
@@ -1,8 +1,68 @@
+import hsa.*;
+import TurtleGraphics.*;
+import java.awt.Color;
+
 
 public class BarGraph {
 
    
     public static void main(String[] args) {
+        Console c = new Console();
+        float val1,val2,val3,val4;
+        SketchPadWindow win = new SketchPadWindow(400,400);
+      Pen p = new StandardPen(win);
+      c.print("Enter your first value (0-200) -> ");
+              val1 = c.readFloat();
+              c.print("Enter your second value (0-200) -> ");
+             val2 = c.readFloat();
+             c.print("Enter your third value (0-200) -> ");
+              val3 = c.readFloat();
+              c.print("Enter your fourth value (0-200) -> ");
+             val4 = c.readFloat();
+       p.up();
+       p.move(-150,150);
+       p.down();
+       p.move(-150,-100);
+       p.turn(90);
+       p.move(300);
+       p.setWidth(20);
+       p.up();
+       p.move(-130,-90);
+       p.down();
+       p.setDirection(90);
+       p.move(val1);
+       p.up();
+       p.move(20);
+       p.drawString("" + val1);
+       
+       p.up();
+       p.move(-80,-90);
+       p.down();
+       p.setDirection(90);
+       p.move(val2);
+       p.up();
+       p.move(20);
+       p.drawString("" + val2);
+       
+       p.up();
+       p.move(-30,-90);
+       p.down();
+       p.setDirection(90);
+       p.move(val3);
+       p.up();
+       p.move(20);
+       p.drawString("" + val3);
+       
+       p.up();
+       p.move(20,-90);
+       p.down();
+       p.setDirection(90);
+       p.move(val4);
+       p.up();
+       p.move(20);
+       p.drawString("" + val4);
+       
+       
        
     }
     
--- src/MakeLine.java
+++ src/MakeLine.java
@@ -1,8 +1,36 @@
+import TurtleGraphics.*;
+import java.awt.Color;
+import hsa.*;
+
+
 
 public class MakeLine {
 
    
     public static void main(String[] args) {
+        float x1,x2,y1,y2;
+        SketchPadWindow win = new SketchPadWindow(400,400);
+      Pen p = new StandardPen(win);
+      Console c = new Console();
+      while(true){
+      c.print("Enter the first X coordinate of your line (0-400) -> ");
+      x1 = c.readFloat();
+      c.print("Enter the first Y coordinate of your line (0-400) -> ");
+      y1 = c.readFloat();
+      c.print("Enter the second X coordinate of your line (0-400) -> ");
+      x2 = c.readFloat();
+      c.print("Enter the second Y coordinate of your line (0-400) -> ");
+      y2 = c.readFloat();
+      if(x1>=0 && x1<=400 && x2>=0 && x2<=400 && y1>=0 && y1<=400 && y2>=0 && y2<=400) break;
+      c.print("Make sure all numbers entered are between 0-400\n");
+      }
+      p.up();
+      p.move(x1-200,y1-200);
+      p.down();
+      p.drawString("(" + x1+ ", " + y1 + ")");
+      p.move(x2-x1, y2-y1);
+      p.drawString("(" + x2+ ", " + y2 + ")" );
+              
       
     }
     
