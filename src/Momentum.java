18758053653d264028297ebf77474523cbe22158
trev8250 <trev8250@ISS147D8-12.scdsb.pri>
2015-09-15 09:34:32 -0400

    pizza

--- src/Momentum.java
+++ src/Momentum.java
@@ -6,12 +6,12 @@ public class Momentum {
     public static void main(String[] args) {
        Console c = new Console();
        double mass,v,mm;
-       c.print("Enter objects mass: ");
+       c.print("Enter objects mass (kg): ");
        mass = c.readDouble();
-       c.print("Enter objects velocity: ");
+       c.print("Enter objects velocity (m/s): ");
        v = c.readDouble();
        mm = mass * v;
-       c.print("The momentum of the object is " + mm);
+       c.print("The momentum of the object is " + mm + " kg m/s");
     }
     
 }
